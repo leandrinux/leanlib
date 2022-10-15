bcefde1060c22148911a685f1c88dd53acc27899
Leandro Tami <leandro.tami@gmail.com>
2022-10-15 00:50:27 -0300

    Improvements to build process

--- .vscode/build.sh
+++ .vscode/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 rundos () {
-  flatpak run com.dosbox_x.DOSBox-X -silent -log-con -fastlaunch -c "cd leanlib" -c "$1" -exit
+  flatpak run com.dosbox_x.DOSBox-X -silent -log-con -fastlaunch -time-limit 15 -c "cd leanlib" -c "$1" -exit
 }
 
-echo Parameter: %1
 rundos "build"
