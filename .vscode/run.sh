cb3ba8953afab38319cc82dfd20fda616ada9ca6
Leandro Tami <leandro.tami@gmail.com>
2022-10-16 11:16:18 -0300

    enabled dosbox-x log while running apps

--- .vscode/run.sh
+++ .vscode/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rundos () {
-  flatpak run com.dosbox_x.DOSBox-X -nolog -nomenu -fastlaunch -c "cd leanlib" -c "$1" -exit
+  flatpak run com.dosbox_x.DOSBox-X -nomenu -fastlaunch -c "cd leanlib" -c "$1" -exit
 }
 
 dospath=`echo $2 | tr / \\`
