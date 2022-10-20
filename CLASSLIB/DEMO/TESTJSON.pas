936b00716ec6d825aaaa4f9a634dc51dd3647c25
Leandro Tami <leandro.tami@gmail.com>
2022-10-20 16:11:21 -0300

    Boolean support for JSON files

---
+++ CLASSLIB/DEMO/TESTJSON.pas
@@ -0,0 +1,30 @@
+uses
+  xcrt, ufstream, ujson, ulist, udict, utils;
+
+var
+  fs: PFileStream;
+  js: PJsonReader;
+  d: PDictionary;
+  l: PList;
+
+begin
+  fs := new(PFileStream, initWithPath('res\example.jsn', true));
+  js := new(PJsonReader, init);
+  d := PDictionary(js^.open(fs));
+  writeln('Name: ', d^.getString('name'));
+  writeln('Age: ', d^.getLong('age'));
+  writeln('Is Alive: ', booltostr(d^.getBool('isAlive')));
+  writeln('Is Evil: ', booltostr(d^.getBool('isEvil')));
+  l := PList(d^.getObject('victims'));
+  writeln('Victim Count: ', inttostr(l^.getCount));
+  writeln('Victim names:');
+  l^.moveToStart;
+  while l^.getObjectAtCursor <> nil do
+  begin
+    writeln('  * ', l^.getStringAtCursor);
+    l^.moveForward;
+  end;
+  js^.release;
+  fs^.release;
+  readkey;
+end.
