e3e0267764e48402097d0a6c978427f10f976187
Leandro Tami <leandro.tami@gmail.com>
2022-12-11 02:00:11 -0300

    Improved scripts to run vscode dev environment from macOS

--- .vscode/buildAll.sh
+++ .vscode/buildAll.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-rundos () {
+run_macos() {
+  /Applications/dosbox-x.app/Contents/MacOS/DosBox -silent -log-con -fastlaunch -time-limit 15 -c "cd leanlib" -c "$1" -exit
+}
+
+run_flatpak () {
   flatpak run com.dosbox_x.DOSBox-X -silent -log-con -fastlaunch -time-limit 15 -c "cd leanlib" -c "$1" -exit
+}
+
+rundos () {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    run_flatpak "$@" 
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    run_macos "$@"
+  fi
 }
 
 rundos "build"
--- .vscode/run.sh
+++ .vscode/run.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-rundos () {
+run_macos () {
+  open /Applications/dosbox-x.app --args -nomenu -fastlaunch -c "cd leanlib" -c "$1" -exit
+}
+
+run_flatpak () {
   flatpak run com.dosbox_x.DOSBox-X -nomenu -fastlaunch -c "cd leanlib" -c "$1" -exit
 }
 
-dospath=`echo $2 | tr / \\`
+rundos () {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    run_flatpak "$@" 
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    run_macos "$@"
+  fi
+}
+
+echo arg $2
+dospath=$(echo $2 | tr / \\)
 filename=$1
 echo Running $filename at $dospath
 rundos "run $dospath $filename"
