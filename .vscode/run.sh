d36e20ebc669c533631aa4eb00799b8c6f3ea78b
Leandro Tami <leandro.tami@gmail.com>
2022-12-12 21:02:34 -0300

    minor fix to run script for macOS

--- .vscode/run.sh
+++ .vscode/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 run_macos () {
-  open /Applications/dosbox-x.app --args -nomenu -fastlaunch -c "cd leanlib" -c "$1" -exit
+  /Applications/dosbox-x.app/Contents/MacOS/DosBox -nomenu -fastlaunch -c "cd leanlib" -c "$1" -exit
 }
 
 run_flatpak () {
