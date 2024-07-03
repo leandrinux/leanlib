f98a502cb28b38fabdff93b901c97c750a2257fc
Leandro Tami <leandro.tami@gmail.com>
2024-07-03 11:36:37 -0300

    Added tcl file to use with WinHex or Hex Fiend to parse binary files

---
+++ LSYSTEM/1.0/LSYSTEM Binary.tcl
@@ -0,0 +1,26 @@
+# Reference Templates
+# https://www.sweetscape.com/010editor/repository/files/BMP.bt
+# https://raw.githubusercontent.com/synalysis/Grammars/master/bitmap.grammar
+
+# Reference Documents
+# http://www.digicamsoft.com/bmp/bmp.html
+# http://en.wikipedia.org/wiki/BMP_file_format
+#
+# .types = ( bin );
+
+little_endian
+
+for {set i 0} {![end]} {incr i} {
+	section "Instruction" {
+		hex 1 "opcode"
+		uint8_bits 7,6,5,4 "param0Type"
+		move -1
+		uint8_bits 3,2,1,0 "param1Type"
+		hex 4 "Arg0 (hex)"
+		move -4 
+		uint32 "Arg0 (dec)"
+		hex 4 "Arg1 (hex)"
+		move -4 
+		uint32 "Arg1 (dec)"
+	}
+}
