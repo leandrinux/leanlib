65335ed415fcc4a0e5e4e6342c082d9860c62306
Leandro Tami <leandro.tami@gmail.com>
2024-07-03 12:04:26 -0300

    much improved tcl file shows parameter types as text

--- LSYSTEM/1.0/LSYSTEM Binary.tcl
+++ LSYSTEM/1.0/LSYSTEM Binary.tcl
@@ -8,6 +8,13 @@
 #
 # .types = ( bin );
 
+set param_types [dict create \
+    0 literal \
+    1 register \
+    2 address \
+    3 pointer \
+]	
+
 little_endian
 requires 0 "4C 53 59 53 42 49 4E"; # requires LSYSBIN
 ascii 7 signature
@@ -15,14 +22,21 @@ ascii 7 signature
 for {set i 0} {![end]} {incr i} {
 	section "Instruction" {
 		hex 1 "opcode"
-		uint8_bits 7,6,5,4 "param0Type"
+
+		set param0Type_n [uint8_bits 7,6,5,4]
+		set param0Type [dict get $param_types $param0Type_n]
+		entry "param 0 type" $param0Type
 		move -1
-		uint8_bits 3,2,1,0 "param1Type"
-		hex 4 "Arg0 (hex)"
+
+		set param0Type_n [uint8_bits 3,2,1,0]
+		set param0Type [dict get $param_types $param0Type_n]
+		entry "param 1 type" $param0Type
+
+		hex 4 "param0 (hex)"
 		move -4 
-		uint32 "Arg0 (dec)"
-		hex 4 "Arg1 (hex)"
+		uint32 "param0 (dec)"
+		hex 4 "param1 (hex)"
 		move -4 
-		uint32 "Arg1 (dec)"
+		uint32 "param1 (dec)"
 	}
 }
